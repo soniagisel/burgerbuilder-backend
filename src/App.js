c0aeec86d0c24f5ac2073b3d70afdbac81d834cc
Sonia Pepe <sogispe@gmail.com>
2020-05-19 21:59:11 -0300

    Added axios and created resquests for GET, POST and DELETE, and also added interceptors and interfaces tu customize the configurations on the requests

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 
 import Blog from './containers/Blog/Blog'
 
-//test comment
 class App extends Component {
   render() {
     return (
--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './index.css'
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
-registerServiceWorker();
+import App from './App'
+import Axios from 'axios'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
+import registerServiceWorker from './registerServiceWorker'
+
+axios.defaults.baseURL = 'http://jsonplaceholder.typicode.com'
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN'
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+
+axios.interceptors.request.use(
+  request => {
+    console.log(request)
+    //it's possible to edit the request config before returning it
+    return request
+  },
+  error => {
+    console.log(error)
+    Promise.reject(error)
+  },
+)
+
+axios.interceptors.response.use(
+  response => {
+    console.log(response)
+    return response
+  },
+  error => {
+    console.log(error)
+    Promise.reject(error)
+  },
+)
+
+//To eject from interceptors simply use eject where needed:
+//var myInterceptor = axios.interceptors.request.use(() => {));
+//axios.interceptors.request.eject(myInterceptor);
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
